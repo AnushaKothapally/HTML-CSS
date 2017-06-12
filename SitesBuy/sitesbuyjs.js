16711c220a0e51e5eb32a46550899e180fbde4b9
Anusha Kothapally <anushayk531@gmail.com>
2017-06-12 09:54:14 -0500

    Add files via upload

---
+++ SitesBuy/sitesbuyjs.js
@@ -0,0 +1,10 @@
+
+function search(){
+   alert("Thank you for searching"); 
+}
+
+function openlink(){
+    
+    document.getElementById('middle').innerHTML='<object type="text/html" data="login.html"></object>';
+    
+}
